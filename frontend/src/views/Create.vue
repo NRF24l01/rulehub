f8282df51a02284599ac6deb6f7c24241aa6ab4b
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-01 12:11:30 +0300

    Use props to get info from editor

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,7 +1,24 @@
 <template>
-  <Editor />
+  <Editor v-model:content="content" v-model:images="images" />
+
+  <div class="mt-4">
+    <h2>Markdown Preview</h2>
+    <div class="markdown-preview">{{ content }}</div>
+  </div>
+  <div class="mt-4">
+    <h2>Uploaded Images</h2>
+    <ul>
+      <li v-for="image in images" :key="image.blobUrl">
+        <img :src="image.blobUrl" :alt="image.name" class="w-32 h-32 object-cover" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import Editor from '@/components/Editor.vue';
+import { ref, watch } from 'vue';
+
+const content = ref('# Пример markdown')
+const images = ref([])
 </script>

54545584b02fe7d7f6e1f581bb4282e05283e6dc
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-10 00:29:04 +0300

    Enhance routing with transition effects in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,5 +5,22 @@ import Navbar from './components/Navbar.vue';
 
 <template>
   <Navbar />
-  <RouterView />
+  <RouterView v-slot="{ Component, route }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" :key="route.fullPath" />
+    </Transition>
+  </RouterView>
 </template>
+
+<style>
+/* Анимация появления/исчезновения */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .25s ease, transform .25s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(6px);
+}
+</style>
